9af8646bc91bf2c32ef8a37fb684545a583dc3bb
ch3tt <71567485+ch3tt@users.noreply.github.com>
2024-10-07 05:29:31 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-    
-    eslint: {
-
-    ignoreDuringBuilds: true,
-  },
-
-    reactStrictMode: false,
+    output: 'standalone',
 };
 
 export default nextConfig;
